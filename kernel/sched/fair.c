9d9cb8d8d8ea0e5126378a815366d4a0929ecde1
ukashah1 <ukashah60@gmail.com>
2021-08-18 04:11:20 +0800

    BACKPORT: sched/fair: Make sched-idle CPU selection consistent throughout

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7045,6 +7045,9 @@ find_idlest_group_cpu(struct sched_group *group, struct task_struct *p, int this
 
 	/* Traverse only the allowed CPUs */
 	for_each_cpu_and(i, sched_group_span(group), &p->cpus_allowed) {
+		if (sched_idle_cpu(i))
+			return i;
+		
 		if (idle_cpu(i)) {
 			struct rq *rq = cpu_rq(i);
 			struct cpuidle_state *idle = idle_get_state(rq);
@@ -7237,6 +7240,7 @@ static int select_idle_smt(struct task_struct *p, struct sched_domain *sd, int t
 		if (!cpumask_test_cpu(cpu, &p->cpus_allowed))
 			continue;
 		if (idle_cpu(cpu))
+		if (idle_cpu(cpu) || sched_idle_cpu(cpu))
 			return cpu;
 	}
 
@@ -7301,6 +7305,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 		if (!--nr)
 			return -1;
 		if (idle_cpu(cpu))
+		if (idle_cpu(cpu) || sched_idle_cpu(cpu))
 			break;
 	}
 
